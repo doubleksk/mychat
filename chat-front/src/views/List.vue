dd59ceaea335cf59f94120e65e6f4b4912fe7273
doubleksk <2633990932@qq.com>
2020-05-07 10:44:37 +0800

    Signed-off-by: doubleksk <2633990932@qq.com>

---
+++ chat-front/src/views/List.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="list">
+        <div class="left">
+            分组区域
+        </div>
+        <div class="right">
+            聊天区域
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['index']
+}
+</script>
+
+<style>
+.list{
+    display: flex;
+    flex-direction: row;
+}
+.left{
+    height: 90vh;
+    background: white;
+    width: 23%;
+    overflow-y: scroll;
+    overflow-x: scroll;
+}
+.right{
+    height: 90vh;
+    background: rgba(236,236,236);
+    flex-grow: 1;
+    overflow: scroll;
+}
+</style>
---
+++ chat-front/src/views/Tab.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="Tab">
+		<el-menu
+			class="el-menu-demo"
+			mode="horizontal"
+			background-color="#353b48"
+			text-color="#b2bec3"
+			active-text-color="#74b9ff"
+			@select="handleSelect">
+			<el-menu-item index="1"><i class="el-icon-tickets"></i>好友</el-menu-item>
+			<el-menu-item index="2"><i class="el-icon-menu"></i>群组</el-menu-item>
+			<el-menu-item index="3"><i class="el-icon-phone-outline"></i>聊天室</el-menu-item>
+			<el-menu-item index="4">陌生人<i class="el-icon-more-outline"></i></el-menu-item>
+		</el-menu>
+		<div class="line"></div>
+		<List :index="index"></List>
+  </div>
+</template>
+
+<script>
+import List from '../views/List'
+export default {
+	name: 'Tab',
+	components: {
+		List
+	},
+	data() {
+		return {
+			activeIndex: '1',
+			index: '1'
+		};
+	  },
+	methods: {
+		handleSelect(key, keyPath) {
+			this.index = key;
+			console.log(this.index);
+		}
+    }
+}
+</script>
+
+<style>
+.Tab {
+    position: fixed;
+	width: 100vw;
+    top: 5vh;
+}
+</style>
